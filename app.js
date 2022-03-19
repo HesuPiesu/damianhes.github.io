867c5a950e1e033daf8e84dd25afc1bb066b0b1b
HesuPiesu <67837411+HesuPiesu@users.noreply.github.com>
2022-03-19 23:01:42 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,124 @@
+/*IMG1 */
+
+function chg()
+{
+    document.getElementById("d1").style.width="55%";
+    document.getElementById("d1").style.height="65%";
+}
+
+function chg2()
+{
+    document.getElementById("d1").style.width="40%";
+    document.getElementById("d1").style.height="50%";
+}
+
+/*START BTN*/
+
+function cht()
+{
+    document.getElementById("d2").style.width="10%";
+    document.getElementById("d2").style.height="12%";
+    document.getElementById("d2").style.fontSize="150%";
+}
+
+function cht2()
+{
+    document.getElementById("d2").style.width="6%";
+    document.getElementById("d2").style.height="8%";
+    document.getElementById("d2").style.fontSize="100%";
+}
+
+/*CREDITS START*/
+function chd()
+{
+    document.getElementById("d3").style.width="6%";
+    document.getElementById("d3").style.height="12%";
+}
+
+function chd2()
+{
+    document.getElementById("d3").style.width="4%";
+    document.getElementById("d3").style.height="8%";
+}
+
+/*CREDITS LIST*/
+function chr()
+{
+    document.getElementById("d4").style.width="30%";
+    document.getElementById("d4").style.height="23%";
+    document.getElementById("d4").style.fontSize="100%";
+}
+
+function chr2()
+{
+    document.getElementById("d4").style.width="25%";
+    document.getElementById("d4").style.height="18%";
+    document.getElementById("d4").style.fontSize="90%";
+}
+
+/*CREDITS WRÓĆ*/
+function chv()
+{
+    document.getElementById("d5").style.width="8%";
+    document.getElementById("d5").style.height="10%";
+    document.getElementById("d5").style.fontSize="100%";
+}
+
+function chv2()
+{
+    document.getElementById("d5").style.width="6%";
+    document.getElementById("d5").style.height="8%";
+    document.getElementById("d5").style.fontSize="90%";
+}
+
+/*2 DALEJ*/
+function chx()
+{
+    document.getElementById("d6").style.width="8%";
+    document.getElementById("d6").style.height="10%";
+    document.getElementById("d6").style.fontSize="100%";
+}
+
+function chx2()
+{
+    document.getElementById("d6").style.width="6%";
+    document.getElementById("d6").style.height="8%";
+    document.getElementById("d6").style.fontSize="90%";
+}
+
+/*HREF*/ 
+
+function btn1()
+{
+    window.location.href = "HTML/index2.html";
+}
+
+function btn10()
+{
+    window.location.href = "index2.html";
+}
+
+function btn2()
+{
+    window.location.href = "index3.html";
+}
+
+function btn3()
+{
+    window.location.href = "index4.html";
+}
+
+function btn4()
+{
+    window.location.href = "index5.html";
+}
+
+function creditf()
+{
+    window.location.href = "HTML/creditf.html";
+}
+
+function babt()
+{
+    window.location.href = "../index.html";
+}
